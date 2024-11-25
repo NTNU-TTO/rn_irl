cca592a4bfa90bbf6134177a775446a99daf77e5
Lodve Berre <160240663+tto-lodve@users.noreply.github.com>
2024-11-25 15:34:24 +0100

    Create install_rn_irl.sh
    Installation script.
    Does not do anything related to SSL - this will have to be set up manually.

---
+++ ubuntu_helpers/install_rn_irl.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Install some basics.
+apt install sqlite3
+apt install python3-venv
+
+# Set up a common rn_irl group for users.
+groupadd -gid 99 rn_irl
+
+# Fetch source code
+git clone https://github.com/NTNU-TTO/rn_irl
+chown root:rn_irl -R rn_irl
+
+# Set up persistent python environment to run in
+python -m venv /etc/rn_irl
+chown root:rn_irl -R rn_irl
+source /etc/rn_irl/bin/activate
+
+# Install required python modules.
+pip install streamlit
+pip install bcrypt
+pip install numpy
+pip install scipy
+pip install matplotlib
+pip install sqlalchemy
+
+# Move the source code inside the virtual python environment.
+mv rn_irl /etc/rn_irl/bin
+
+# Move the database to a safe location.
+mkdir /var/lib/rn_irl
+mv /etc/rn_irl/bin/rn_irl/irl.db /var/lib/rn_irl
+
+# Update default database path.
+sed -i -e 's#db_path = sqlite:////irl.sdb#db_path = sqlite:////var//lib//rn_irl//irl.sdb#g' /etc/rn_irl/bin/rn_irl/.streamlit/secrets.toml
+
+# Create symlink for executable
+ln -s /etc/rn_irl/bin/rn_irl/ubuntu_helpers/rn_irl.sh /bin/rn_irl
+
+# Copy service
+cp /etc/rn_irl/bin/rn_irl/ubuntu_helpers/rn_irl.service /lib/systemd/system/
+
+#Reload, enable and run service
+systemctl daemon-reload
+systemctl enable rn_irl
+service rn_irl start
