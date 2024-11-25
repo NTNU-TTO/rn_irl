acc3497b9a5a8e018c474bd13feeb9b5f7cfae21
Lodve Berre <160240663+tto-lodve@users.noreply.github.com>
2024-11-25 14:41:32 +0100

    Create rn_irl_staging2prod.sh
    Bash script for moving the staging environment to production.

---
+++ ubuntu_helpers/rn_irl_staging2prod.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Shut down staging service.
+service rn_irl_staging stop
+
+# Change all paths in the staging environment to the production environment path.
+cd /etc/rn_irl_staging/
+sed -i -e 's/rn_irl_staging/rn_irl/g' pyenv.cfg
+
+cd /etc/rn_irl_staging/bin
+
+for entry in *
+
+do
+  if [ -f "$entry" ];then
+    sed -i -e 's/rn_irl_staging/rn_irl/g' "$entry"
+  fi
+done
+
+# Overwrite port config - move back to port 443.
+sed -i -e 's/port = 8501/port = 443/g' /etc/rn_irl_staging/bin/rn_irl/.streamlig/config.toml
+
+# Shut down production enviorment.
+service rn_irl stop
+
+# Back up current production environment.
+DATE=$(date -I)
+mv /etc/rn_irl /etc/rn_irl_$DATE
+
+# Move staging enviroment path to production.
+mv /etc/rn_irl_staging /etc/rn_irl
+cp /etc/rn_irl/bin/ubuntu_helpers/rn_irl.sh /etc/rn_ril/bin/rn_irl.sh
+
+# Restart production environment.
+service rn_irl start
+
+# Remove staging symlink.
+rm /bin/rn_irl_staging
