017d607d9e8a0cc5b8c25b52b761f43109d04fb3
Lodve Berre <160240663+tto-lodve@users.noreply.github.com>
2024-11-26 15:43:02 +0100

    Update rn_irl_staging.sh

--- ubuntu_helpers/rn_irl_staging.sh
+++ ubuntu_helpers/rn_irl_staging.sh
@@ -31,7 +31,7 @@ sed -i -e 's/port = 443/port = 8501/g' /etc/rn_irl_staging/bin/rn_irl/.streamlit
 chown root:rn_irl -R /etc/rn_irl_staging
 
 # Create symlink
-ln -sf /etc/rn_irl_staging/bin/rn_irl/ubuntu_helpers/rn_irl_staging.sh /bin/rn_irl_staging
+ln -sf /etc/rn_irl_staging/bin/rn_irl/ubuntu_helpers/rn_irl_staging_env.sh /bin/rn_irl_staging
 # Copy service
 cp /etc/rn_irl_staging/bin/rn_irl/ubuntu_helpers/rn_irl_staging.service /lib/systemd/system
 
