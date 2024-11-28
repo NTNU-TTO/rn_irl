293172c2d917ff079f363adfef54247dd68ca28a
Lodve Berre <160240663+tto-lodve@users.noreply.github.com>
2024-11-28 13:44:22 +0100

    Update rn_irl_staging.sh
    Adding show_valuations column in System Settings table if not present.

--- ubuntu_helpers/rn_irl_staging.sh
+++ ubuntu_helpers/rn_irl_staging.sh
@@ -37,6 +37,17 @@ ln -sf /etc/rn_irl_staging/bin/rn_irl/ubuntu_helpers/rn_irl_staging_env.sh /bin/
 # Copy service
 cp /etc/rn_irl_staging/bin/rn_irl/ubuntu_helpers/rn_irl_staging.service /lib/systemd/system
 
+# Make sure database is up to date.
+HAS_COL=$(sqlite3 rn_irl/irl.sdb "select show_valuations from 'system settings';")
+
+if [[ $HAS_COL == "0" ]] || [[ $HAS_COL == "1" ]]; then
+        echo "RN IRL Database System Settings up to date, moving on..."
+else
+        echo "RN IRL Database System Settings table not up to date, adding column show_valuations."
+        sqlite3 /var/lib/rn_irl/irl.sdb 'ALTER TABLE "System Settings" ADD COLUMN show_valuations INTEGER(1);'
+        sqlite3 /var/lib/rn_irl/irl.sdb 'UPDATE "System Settings" SET show_valuations = 0;'
+fi
+
 # Reload, enable and run service.
 echo "Starting staging environment..."
 systemctl daemon-reload
