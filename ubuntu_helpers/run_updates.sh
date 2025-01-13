8a65d4eb601d82c0a95e4b22b0c24a9a18cf4025
Lodve Berre <160240663+tto-lodve@users.noreply.github.com>
2025-01-13 20:26:48 +0100

    Update run_updates.sh
    Added settings for automagic e-mail handling when new user added without password.

--- ubuntu_helpers/run_updates.sh
+++ ubuntu_helpers/run_updates.sh
@@ -10,3 +10,23 @@ else
         sqlite3 /var/lib/rn_irl/irl.sdb 'ALTER TABLE "System Settings" ADD COLUMN show_valuations INTEGER(1);'
         sqlite3 /var/lib/rn_irl/irl.sdb 'UPDATE "System Settings" SET show_valuations = 0;'
 fi
+
+HAS_COL=$(sqlite3 /var/lib/rn_irl/irl.sdb "select noreply_address from 'System Settings';")
+
+if [[ $HAS_COL == "0" ]] || [[ $HAS_COL == "1" ]]; then
+        echo "RN IRL Database System Settings up to date, moving on..."
+else
+        echo "RN IRL Database System Settings table not up to date, adding column noreply_address."
+        sqlite3 /var/lib/rn_irl/irl.sdb 'ALTER TABLE "System Settings" ADD COLUMN noreply_address TEXT();'
+        sqlite3 /var/lib/rn_irl/irl.sdb 'UPDATE "System Settings" SET noreply_address = "noreply@rn-irl.tto.ntnu.no";'
+fi
+
+HAS_COL=$(sqlite3 /var/lib/rn_irl/irl.sdb "select noreply_body from 'System Settings';")
+
+if [[ $HAS_COL == "0" ]] || [[ $HAS_COL == "1" ]]; then
+        echo "RN IRL Database System Settings up to date, moving on..."
+else
+        echo "RN IRL Database System Settings table not up to date, adding column noreply_body."
+        sqlite3 /var/lib/rn_irl/irl.sdb 'ALTER TABLE "System Settings" ADD COLUMN noreply_body TEXT();'
+        sqlite3 /var/lib/rn_irl/irl.sdb 'UPDATE "System Settings" SET noreply_body = "Welcome to Really Nice IRL!\n\nWe have generated a login password for you: %s\n\nWe suggest you change this upon your first login.\nGood luck with your innovation project going forward!\n\nBest regards,\nThe Really Nice IRL Team";'
+fi
