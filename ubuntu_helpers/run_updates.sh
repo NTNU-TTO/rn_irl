45fe684f0c5a32a8e4dc3b28dd5c7022c169cb81
Lodve Berre <160240663+tto-lodve@users.noreply.github.com>
2025-01-14 13:41:43 +0100

    Update run_updates.sh
    Fixed typos.

--- ubuntu_helpers/run_updates.sh
+++ ubuntu_helpers/run_updates.sh
@@ -17,7 +17,7 @@ if [[ $HAS_COL == "0" ]] || [[ $HAS_COL == "1" ]]; then
         echo "RN IRL Database System Settings up to date, moving on..."
 else
         echo "RN IRL Database System Settings table not up to date, adding column noreply_address."
-        sqlite3 /var/lib/rn_irl/irl.sdb 'ALTER TABLE "System Settings" ADD COLUMN noreply_address TEXT();'
+        sqlite3 /var/lib/rn_irl/irl.sdb 'ALTER TABLE "System Settings" ADD COLUMN noreply_address TEXT;'
         sqlite3 /var/lib/rn_irl/irl.sdb 'UPDATE "System Settings" SET noreply_address = "noreply@rn-irl.tto.ntnu.no";'
 fi
 
@@ -27,6 +27,6 @@ if [[ $HAS_COL == "0" ]] || [[ $HAS_COL == "1" ]]; then
         echo "RN IRL Database System Settings up to date, moving on..."
 else
         echo "RN IRL Database System Settings table not up to date, adding column noreply_body."
-        sqlite3 /var/lib/rn_irl/irl.sdb 'ALTER TABLE "System Settings" ADD COLUMN noreply_body TEXT();'
+        sqlite3 /var/lib/rn_irl/irl.sdb 'ALTER TABLE "System Settings" ADD COLUMN noreply_body TEXT;'
         sqlite3 /var/lib/rn_irl/irl.sdb 'UPDATE "System Settings" SET noreply_body = "Welcome to Really Nice IRL!\n\nWe have generated a login password for you: %s\n\nWe suggest you change this upon your first login.\nGood luck with your innovation project going forward!\n\nBest regards,\nThe Really Nice IRL Team";'
 fi
