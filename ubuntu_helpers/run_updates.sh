1b574fd69dff7ef08967a818580a20aa9584f0ed
seljudas <lodve.berre@gmail.com>
2026-04-27 22:55:00 +0200

    Fixed version typo.

--- ubuntu_helpers/run_updates.sh
+++ ubuntu_helpers/run_updates.sh
@@ -3,8 +3,8 @@
 # Update database if needed.
 ERROR="no such column"
 IRL_REV="Version F released 2025"
-APP_VER="0.6.260427"
-DB_VER="0.6.260427"
+APP_VERSION="0.6.260427"
+DB_VERSION="0.6.260427"
 IRL_PROD_DB="/var/lib/rn_irl/irl.sdb"
 IRL_GIT_DB="/etc/rn_irl_staging/bin/rn_irl/irl.sdb"
 HAS_COL=$(sqlite3 "$IRL_PROD_DB" "SELECT show_valuations FROM 'System Settings';" 2>&1)
